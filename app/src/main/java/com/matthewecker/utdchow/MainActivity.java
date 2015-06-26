c510c4cad321d918f5bfeaf085f5f9cf63083035
gutlessVADER <matthew@Matthews-MacBook-Pro.local>
2015-06-26 13:17:24 -0500

    Initial commit
    has some buttons, most work
    
    styling needed

---
+++ app/src/main/java/com/matthewecker/utdchow/MainActivity.java
@@ -0,0 +1,54 @@
+package com.matthewecker.utdchow;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            // openSettings();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void viewOnCampus(View view) {
+        Intent intent = new Intent(this, OnCampusActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewOffCampus(View view) {
+        Intent intent = new Intent(this, OffCampusActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewMonster(View view) {
+        Intent intent = new Intent(this, MonsterActivity.class);
+        startActivity(intent);
+    }
+}
